676d8540ee42cf0d5612eecb0d039f0bba5449b6
Konstantinos Orfeas Kiourtidis <orfeaskiourtidis@hotmail.com>
2023-01-09 12:50:51 +0200

    Error Handlers & Mappers Addition

---
+++ src/main/java/com/team5/ACMEFlix/repository/BackupRepository.java
@@ -0,0 +1,18 @@
+package com.team5.ACMEFlix.repository;
+
+import com.team5.ACMEFlix.domain.Address;
+import com.team5.ACMEFlix.domain.Backup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface BackupRepository extends JpaRepository<Backup, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "BACKUP TO 'backup.zip'", nativeQuery = true)
+    int backupDB(String path);
+}
---
+++ src/main/java/com/team5/ACMEFlix/service/DatabaseBackupService.java
@@ -0,0 +1,28 @@
+package com.team5.ACMEFlix.service;
+
+import com.team5.ACMEFlix.domain.Backup;
+import com.team5.ACMEFlix.repository.BackupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class DatabaseBackupService {
+
+    @Autowired
+    private BackupRepository backupRepository;
+
+    public DatabaseBackupService(BackupRepository backupRepository) {
+        this.backupRepository = backupRepository;
+    }
+
+    @Transactional
+    public int backup(String path) {
+        Backup backup = new Backup();
+        backupRepository.save(backup);
+        return this.backupRepository.backupDB(path);
+
+    }
+
+}
